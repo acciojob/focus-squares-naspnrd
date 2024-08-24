1cfc30157821b02d6eebd7ce04cc98282466b35e
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-08-24 13:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const squares = document.querySelectorAll(".square");
-squares.forEach((sqaure) => {
-	sqaure.addEventListener("mouseover", handleMouseOver)
-	sqaure.addEventListener("mouseout", handleMouseOut)
+
+squares.forEach((square) => {
+	square.addEventListener("mouseover", handleMouseOver)
+	square.addEventListener("mouseout", handleMouseOut)
 })
 
-function handleMouseOver(event){
-	squares.forEach((square) => {
-		if(square !== event.target){
-			square.style.backgroundColor = "#6F4E37"; // coffee color
+function handleMouseOver(event) {
+	// event.target -> where the cursor is
+	// html element
+	squares.forEach((sqaure) => {
+		if(sqaure !== event.target) {
+			sqaure.style.backgroundColor = "#6F4E37";
 		}
 	})
 }
 
-function handleMouseOut(event){
+function handleMouseOut() {
 	squares.forEach((square) => {
-		square.style.backgroundColor = "#E6E6FA"; // lavendar color
-	})
+		square.style.backgroundColor = "#E6E6FA";	
+	});
 }
 
