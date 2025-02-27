f35426d5dbdf075251551defebde1fd46e4ec3fd
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2025-02-27 23:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
+// const squares = document.querySelectorAll(".square");
+
+// squares.forEach((square) => {
+// 	square.addEventListener("mouseover", handleMouseOver)
+// 	square.addEventListener("mouseout", handleMouseOut)
+// })
+
+// function handleMouseOver(event) {
+// 	// event.target -> where the cursor is
+// 	// html element
+// 	squares.forEach((sqaure) => {
+// 		if(sqaure !== event.target) {
+// 			sqaure.style.backgroundColor = "#6F4E37";
+// 		}
+// 	})
+// }
+
+// function handleMouseOut() {
+// 	squares.forEach((square) => {
+// 		square.style.backgroundColor = "#E6E6FA";	
+// 	});
+// }
+
 const squares = document.querySelectorAll(".square");
 
-squares.forEach((square) => {
-	square.addEventListener("mouseover", handleMouseOver)
-	square.addEventListener("mouseout", handleMouseOut)
-})
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        squares.forEach(s => s.style.backgroundColor = s === square ? "#E6E6FA" : "#6F4E37");
+    });
 
-function handleMouseOver(event) {
-	// event.target -> where the cursor is
-	// html element
-	squares.forEach((sqaure) => {
-		if(sqaure !== event.target) {
-			sqaure.style.backgroundColor = "#6F4E37";
-		}
-	})
-}
-
-function handleMouseOut() {
-	squares.forEach((square) => {
-		square.style.backgroundColor = "#E6E6FA";	
-	});
-}
+    square.addEventListener("mouseout", () => {
+        squares.forEach(s => s.style.backgroundColor = "#E6E6FA");
+    });
+});
 
